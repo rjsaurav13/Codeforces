bf334f83a42faa60f0669c4f563162dd9a51e7ca
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-04-27 15:09:05 +0530

    Create Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num%2==0&&num>2){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+
+}
